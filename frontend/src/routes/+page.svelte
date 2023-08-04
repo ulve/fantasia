5ec255e6d0503d2d6d744631f1f2c6f8ee8b12ae
Olov Johansson <johansson.olov@gmail.com>
2023-08-04 15:04:06 +0200

    titel casing

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -57,7 +57,7 @@
 				</p>
 			</div>
 			<div class="logo">
-				<a href={product.link} target="_blank">
+				<a href={product.url} target="_blank">
 					<img src="logo.png" alt="Logo" width="100%" />
 				</a>
 			</div>
